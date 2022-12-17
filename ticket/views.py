301c17c9270e124e6ebc114a895ff6d866dc0bc6
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-12-17 15:15:21 +0100

    TemplateView ok

--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
 # Create your views here.
+
+class OView(TemplateView):
+    template_name = 'ticket/index.html'
