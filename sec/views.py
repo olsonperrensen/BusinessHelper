d97c0aa3f267517ae59be1f60621095f566ed5fc
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-12-20 14:20:59 +0100

    login accepts superuser

--- sec/views.py
+++ sec/views.py
@@ -5,4 +5,13 @@ from django.contrib import messages
 
 
 def login_user(req):
-    return render(req,'authenticate/login.html')
+    if req.method == 'POST':
+        user = authenticate(req, username=req.POST['em'], password=req.POST['pwd'])
+        if user is not None:
+            login(req, user)
+            return redirect('homepage')
+        else:
+            messages.success(req, ("Invalid. Try again"))
+            return redirect('login')
+
+    return render(req, 'authenticate/login.html')
