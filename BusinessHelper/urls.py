4d9cbf270ce4a8e24fc06a8e34554ddc13292a72
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-12-20 14:03:56 +0100

    dummy login working

--- BusinessHelper/urls.py
+++ BusinessHelper/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls,name='admin'),
-    path('',include('ticket.urls'),name='index')
+    path('admin/', admin.site.urls, name='admin'),
+    path('', include('ticket.urls'), name='index'),
+    path('login/', include('django.contrib.auth.urls')),
+    path('login/', include('sec.urls'))
 ]
