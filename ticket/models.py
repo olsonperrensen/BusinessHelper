a773b00ecaa1453828482a3871792967f6944a4a
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-12-22 15:45:48 +0100

    image upload working

--- ticket/models.py
+++ ticket/models.py
@@ -11,7 +11,11 @@ class Ticket(models.Model):
         #{self.pk} by {self.author} has {self.priority} priority
         """
 
-    priority = models.CharField(max_length=6, default='Low')
+    # HIDDEN INPUT FIELDS SO IT BECOMES EASIER TO IDENTIFY USER
     author = models.CharField(max_length=80, default='John Smith')
     date = models.DateTimeField(auto_now=True)
+    # PUBLICLY DISPLAYED FIELDS
+    priority = models.CharField(max_length=6, default='Low')
+    evidence = models.ImageField(upload_to="images",
+                                 default='https://oncall258.com/wp-content/uploads/2015/03/iStock_000054981982_Small.jpg')
     comment = models.CharField(max_length=5000, default='Lorem Ipsum')
